c2a1945b93d8dd835b87326fbe5c0c4a7d4fb877
RaisaKemppainen <raisa.kemppainen@brightstraining.com>
2022-11-09 10:01:28 +0100

    Git test merge

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,7 @@ public class Main {
 		System.out.println("Change 3");
 		System.out.println("Changed Clone");
 		System.out.println("Changed clone after original");
+		System.out.println("Changed clone after original");
+
 	}
 }
