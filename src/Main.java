c11fc7df2ef43ef1a366c1c5588127fdde08837b
RaisaKemppainen <raisa.kemppainen@brightstraining.com>
2022-11-09 10:05:04 +0100

    Changed clone again

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,6 @@ public class Main {
 		System.out.println("Changed Clone");
 		System.out.println("Changed clone after original");
 		System.out.println("Changed clone after original");
-
+		System.out.println("Changed clone after original 3 Test");
 	}
 }
